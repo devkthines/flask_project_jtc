e630887a5125addd2d24827e358d89d46268fba5
dev.kthines <dev.kthines@gmail.com>
2024-03-08 20:48:20 -0800

    Need to add some interests to the html over the weekend.

---
+++ static/js/main.js
@@ -0,0 +1,13 @@
+﻿function changeBackgroundColor(){
+
+    var colors =['#ff0000', '#00ff00', '#0000ff', '#ffff00', '#00ffff']
+    var randomColor = colors[Math.floor(Math.random()*colors.length)]
+    document.body.style.backgroundColor = randomColor;
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+    var button = document.getElementById('changeColorButton');
+    if(button){
+        button.addEventListener('click', changeBackgroundColor);
+    }
+})
